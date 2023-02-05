8d15c2b7cdac0256b326555645ab4658eabd1c7a
David Sehi <david.sehi2016@gmail.com>
2023-02-05 16:46:44 +0000

    Update headers

--- api/todos/update.php
+++ api/todos/update.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: PUT, POST');
 header('Content-Type: application/json');
 
 require_once '../../config/Database.php';
