c6f8427db1d58db657340a98448e7434b7cd2cfd
David Sehi <david.sehi2016@gmail.com>
2023-02-05 16:41:23 +0000

    Add `update` method

--- models/TodoModel.php
+++ models/TodoModel.php
@@ -19,13 +19,17 @@ class Todo
     // sql query
     $sql = "SELECT todoId, todo, createdAt, updatedAt
         FROM {$this->table} WHERE userId = ? ORDER BY id ASC";
-
-    // prepare statement
     $stmt = $this->connection->prepare($sql);
-
-    // execute
     $stmt->execute([$userId]);
+    return $stmt;
+  }
 
+  public function update(string $todo, string $todoId)
+  {
+    $sql =
+      'UPDATE todo SET todo = ?, updatedAt = CURRENT_TIMESTAMP WHERE todoId = ?';
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute([$todo, $todoId]);
     return $stmt;
   }
 }
